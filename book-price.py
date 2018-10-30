e4137af36526c1d0ed80c45b9aa9cf35078b8609
ciyank <jameslandor@live.com>
2018-10-30 14:12:56 -0400

    change using MO awesome code

--- book-price.py
+++ book-price.py
@@ -1,7 +1,7 @@
 book_price = 24.95
-discount = book_price * .4
-discount_price = book_price - discount
-shipping_cost_first = 3
-shipping_cost_rem = 0.75
-total_cost = discount_price * 60 + shipping_cost_first + shipping_cost_rem * 59
+discount_price = book_price * .6
+book_count = float (input('Number of copies purchased'))
+total_cost = discount_price * book_count
+shipping_cost = 3 + (.75 * (book_count -1))
+total = total_cost + shipping_cost
 print (total_cost)
