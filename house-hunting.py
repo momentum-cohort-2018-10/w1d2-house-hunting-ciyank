052e5f83b6b6ebc7dbc43f1e9ee35774703c8462
ciyank <jameslandor@live.com>
2018-10-30 17:15:26 -0400

    finally correct code

--- house-hunting.py
+++ house-hunting.py
@@ -1,17 +1,15 @@
-annual_salary = float (input('Enter you annual salary'))
-portion_saved = float (input ('Enter the percent of your salary to save, as a decimal'))
-total_cost = float (input('Enter the cost of your dream home'))
-num_of_months = 1
+annual_salary = float (input('Enter you annual salary '))
+portion_saved = float (input ('Enter the percent of your salary to save, as a decimal '))
+total_cost = float (input('Enter the cost of your dream home '))
+num_of_months = 0
 portion_down_payment = total_cost*.25
 current_savings = 0
 r = .04
 
-current_savings = current_savings + (current_savings*r/12) + (annual_salary/12*portion_saved)
-
 while current_savings < portion_down_payment:
     num_of_months += 1
-    if current_savings >= portion_down_payment:
-        print ("number of month", num_of_months)
+    current_savings = current_savings + (current_savings*r/12) + ((annual_salary/12)*portion_saved)
+print ('num of months ' + str(num_of_months))
 
 
 
