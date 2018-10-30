f43d312b943448db3e3b93f10a7180fab66ac521
ciyank <jameslandor@live.com>
2018-10-30 12:32:08 -0400

    updated errors

--- house-hunting.py
+++ house-hunting.py
@@ -1,6 +1,6 @@
 annual_salary = float (input('Enter you annual salary'))
 percent_of_salary = float (input ('Enter the percent of your salary to save, as a decimal'))
 cost_of_dream_home = float (input(' Enter the cost of your dream home'))
-print cost_of_dream_home
-print percent_of_salary
-print cost_of_dream_home
+print (cost_of_dream_home)
+print (percent_of_salary)
+print (cost_of_dream_home)
