75f4f5cfc252ee0d5ff5f2b1327a72a498729020
ciyank <jameslandor@live.com>
2018-10-30 13:36:14 -0400

    first code

---
+++ book-price.py
@@ -0,0 +1,7 @@
+book_price = 24.95
+discount = book_price * .4
+discount_price = book_price - discount
+shipping_cost_first = 3
+shipping_cost_rem = 0.75
+total_cost = discount_price + shipping_cost_first + shipping_cost_rem * 59
+print (total_cost)
