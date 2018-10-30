5da8986e1505d4cfeba500f9fb5f5a0fbc6c86d4
ciyank <jameslandor@live.com>
2018-10-30 14:16:41 -0400

    fixed bug

--- book-price.py
+++ book-price.py
@@ -4,4 +4,4 @@ book_count = float (input('Number of copies purchased'))
 total_cost = discount_price * book_count
 shipping_cost = 3 + (.75 * (book_count -1))
 total = total_cost + shipping_cost
-print (total_cost)
+print (total)
