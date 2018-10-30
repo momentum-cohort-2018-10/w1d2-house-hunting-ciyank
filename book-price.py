9802986eb6c9c211ab6539b6bd94a4975341252b
ciyank <jameslandor@live.com>
2018-10-30 13:38:57 -0400

    code update

--- book-price.py
+++ book-price.py
@@ -3,5 +3,5 @@ discount = book_price * .4
 discount_price = book_price - discount
 shipping_cost_first = 3
 shipping_cost_rem = 0.75
-total_cost = discount_price + shipping_cost_first + shipping_cost_rem * 59
+total_cost = discount_price * 60 + shipping_cost_first + shipping_cost_rem * 59
 print (total_cost)
